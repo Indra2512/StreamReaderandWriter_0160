f1c65fd9fb12323e5c44040201a816bbac5047de
Indra2512 <mononeon1225@gmail.com>
2025-06-09 14:35:50 +0700

    menambahkan looping

--- ioFile.cpp
+++ ioFile.cpp
@@ -14,6 +14,22 @@ int main()
 
      cout << ">= menulis file, \'q\' untuk keluar" << endl;
 
+     //unlimited loop untuk menulis
+     while (true)
+     {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+     }
+     //selesai dalam menulis sekarang tutup filenya
+     outfile.close();
 
+     //membuat objek input file stream
+     ifstream infile;
+     
 };
    
