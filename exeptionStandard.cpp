b4fcd4b5b3012ad54462f846f5d6f112b27f17a0
Indra2512 <mononeon1225@gmail.com>
2025-06-09 15:27:04 +0700

    menambahkan fungsi pada exeptionStandard

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -4,3 +4,20 @@
 #include <array>
 //untuk obyek array uang akan kita gunakan
 using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:...
+    try
+    {
+        array<int, 3> data = { 10, 20, 30 };
+        //pesan array integer 3 elemen
+        cout<<data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
+    catch (exception& e)
+    {
+        //penangkap menggunakan obyek exception
+        
+    }
+}
