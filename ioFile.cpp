a68ad176f3a6957c585022235af1cdda778347e7
Indra2512 <mononeon1225@gmail.com>
2025-06-09 14:44:16 +0700

    melakukan perulangan pada setiap barisnya

--- ioFile.cpp
+++ ioFile.cpp
@@ -30,6 +30,22 @@ int main()
 
      //membuat objek input file stream
      ifstream infile;
-     
+
+     //membuka file yang dituliskan
+     infile.open("contohfile.txt");
+
+     cout << endl << ">= membuka dan membaca file " << endl;
+     //jika file ada maka
+     if (infile.is_open())
+     {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+     }
 };
    
